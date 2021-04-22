e93dd953ec904a23b278b4fe2c40ac7851d27655
AmanPathak8184 <amanpathak8184@gmail.com>
2021-04-22 17:25:11 +0530

    6

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -8,7 +8,7 @@ class Carousel extends Component {
         <div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel" >
           <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="5000">
-              <img src="Images/Carousel/1.jpg" class="d-block w-100 ImageRound" alt="..." />
+              <img src="Images/Carousel/C1.jpg" class="d-block w-100 ImageRound" alt="..." />
             </div>
             <div class="carousel-item" data-bs-interval="5000">
               <img src="Images/Carousel/2.jpg" class="d-block w-100 ImageRound" alt="..." />
