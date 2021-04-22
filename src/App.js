9108f4c72985f2b6cf2f502b81331697824642cc
AmanPathak8184 <amanpathak8184@gmail.com>
2021-04-22 09:33:17 +0530

    3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import React from 'react';
 import Header from './Components/Header';
 import Cards from './Components/Cards';
+import Carousel from './Components/Carousel';
 function App() {
   return (
     <div>
       <Header />
 
+     
+
       <section className="container-fluid  ">
         
+      <Carousel />
+
         <div className="row d-flex justify-content-center">
 
           {/* This Is Card 1 */}
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -7,7 +7,7 @@ class Cards extends Component {
 
             <div className="card" id="Cards">
                 <img src="/Images/1.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
+                <div className="card-body ">
                     <h3 className="card-title">Tekken 7 has reached another milestone</h3>
                     <p className="text-muted ">Tekken 7 has reached another milestone with over 7 MILLION sales worldwide!
                       Thank You to all our fans for their continued support, now get ready for the next battle!</p>
