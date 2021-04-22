1b4818d71d9a5507e6253d452c4894ee3736cf71
AmanPathak8184 <amanpathak8184@gmail.com>
2021-04-22 16:41:48 +0530

    Carousel Added

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -5,7 +5,7 @@ class Cards extends Component {
     render() {
         return (
 
-            <div className="card" id="Cards">
+            <div className="card " id="Cards">
                 <img src="/Images/1.jpg" className="card-img-top" alt="..." />
                 <div className="card-body ">
                     <h3 className="card-title">Tekken 7 has reached another milestone</h3>
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,18 +1,19 @@
+import './Carousel.css'
 import React, { Component } from 'react';
 
 class Carousel extends Component {
     render() {
         return (
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="Images/Carousel/1.jpg" class="d-block w-100" alt="..."/>
+            <div id="carouselExampleSlidesOnly" class=" carousel slide" data-bs-ride="carousel " id="Carousel1">
+            <div class="carousel-inner ">
+              <div class="carousel-item active ">
+                <img src="Images/Carousel/1.jpg" className="d-block w-100 ImageRound" alt="..."/>
               </div>
               <div class="carousel-item">
-                <img src="Images/1.jpg" class="d-block w-100" alt="..."/>
+                <img src="Images/1.jpg" className="d-block w-100 rounded" alt="..."/>
               </div>
               <div class="carousel-item">
-                <img src="Images/Carousel/1.jpg" class="d-block w-100" alt="..."/>
+                <img src="Images/Carousel/1.jpg" className="d-block w-100 rounded" alt="..."/>
               </div>
             </div>
           </div>
