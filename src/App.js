fddb4c2264e63db0d76ccc1c9af85f4bb1afd02d
AmanPathak8184 <amanpathak8184@gmail.com>
2021-04-23 23:33:57 +0530

    26

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div>
       <Header />
 
-      <HeadingText heading="Featured-24" /> 
+      <HeadingText heading="Featured-26" /> 
 
       <section className="container-fluid  ">
         
@@ -29,6 +29,13 @@ function App() {
                 cardHeading="MotoGP21 is now available."
                 shortDescription="The time has come! Feel the thrills of the most celebrate bikes, the
                 the emotions of the greatest champions, and face challenges like never before!"
+        />
+
+        <Cards  imageLink="/Images/Cards/Card3.jpg"
+                cardHeading="Horizon Zero Dawn Sale!"
+                shortDescription="You've got 48 hours left to save big on Horizon Zero Dawn for PC.
+                Grab the Complete Edition for $29.99 to uncover Aloy's destiny, but be quick - this sale
+                ends on April 25!"
         />
 
         </div>
