6139883587f9c99433b98ba46656953f17fc0d89
AmanPathak8184 <amanpathak8184@gmail.com>
2021-04-25 01:58:53 +0530

    29

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
 import React from 'react';
 import Header from './Components/Header';
-
 import Carousel from './Components/Carousel';
 import ImageCard from './Components/ImageCard';
 import HeadingText from './Components/HeadingText';
-
 import CardSection from './Components/CardsSection';
 
 
 function App() {
 
-
-
   return (
     <div>
       <Header />
 
-      <HeadingText heading="Featured-27" /> 
+      <HeadingText heading="Featured-28" /> 
 
       <CardSection />
 
